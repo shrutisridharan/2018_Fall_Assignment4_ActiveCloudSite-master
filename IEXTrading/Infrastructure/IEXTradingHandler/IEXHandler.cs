de0c224b471ac68ca39015e7b12aa9ee06b77a21
shrutisridharan <43157773+shrutisridharan@users.noreply.github.com>
2018-11-11 02:22:49 -0500

    Added files

---
+++ IEXTrading/Infrastructure/IEXTradingHandler/IEXHandler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using IEXTrading.Models;
+using Newtonsoft.Json;
+
+namespace IEXTrading.Infrastructure.IEXTradingHandler
+{
+    public class IEXHandler
+    {
+        static string BASE_URL = "https://api.iextrading.com/1.0/"; //This is the base URL, method specific URL is appended to this.
+        HttpClient httpClient;
+
+        public IEXHandler()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /****
+         * Calls the IEX reference API to get the list of symbols. 
+        ****/
+        public List<Company> GetSymbols()
+        {
+            string IEXTrading_API_PATH = BASE_URL + "ref-data/symbols";
+            string companyList = "";
+
+            List<Company> companies = null;
+
+            httpClient.BaseAddress = new Uri(IEXTrading_API_PATH);
+            HttpResponseMessage response = httpClient.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                companyList = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+
+            if (!companyList.Equals(""))
+            {
+                companies = JsonConvert.DeserializeObject<List<Company>>(companyList);
+                companies = companies.GetRange(0, 9);
+            }
+            return companies;
+        }
+
+        /****
+         * Calls the IEX stock API to get 1 year's chart for the supplied symbol. 
+        ****/
+        public List<Equity> GetChart(string symbol)
+        {
+            //Using the format method.
+            //string IEXTrading_API_PATH = BASE_URL + "stock/{0}/batch?types=chart&range=1y";
+            //IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
+
+            string IEXTrading_API_PATH = BASE_URL + "stock/" + symbol + "/batch?types=chart&range=1y";
+
+            string charts = "";
+            List<Equity> Equities = new List<Equity>();
+            httpClient.BaseAddress = new Uri(IEXTrading_API_PATH);
+            HttpResponseMessage response = httpClient.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                charts = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+            if (!charts.Equals(""))
+            {
+                ChartRoot root = JsonConvert.DeserializeObject<ChartRoot>(charts, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                Equities = root.chart.ToList();
+            }
+            //make sure to add the symbol the chart
+            foreach (Equity Equity in Equities)
+            {
+                Equity.symbol = symbol;
+            }
+
+            return Equities;
+        }
+    }
+}
